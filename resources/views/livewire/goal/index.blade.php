d5654a7ac783168afee91a17ce5915c4f4f0f1a5
Laylatul Firdaus <laylatul.firdaus@outlook.com>
2024-02-12 00:48:39 +0000

    Fixed Completed Checkbox Issue

--- resources/views/livewire/goal/index.blade.php
+++ resources/views/livewire/goal/index.blade.php
@@ -12,25 +12,30 @@
         <div class="goal mt-3 p-3 {{ $goal->completed ? 'completed' : '' }}" id="goal">
             <div class="row">
                 <div class="col-md-1">
-                    <input type="checkbox" wire:change="toggleGoal({{ $goal->id }})">
+                    <input wire:click="completedGoal({{ $goal->id }})" type="checkbox" id="myCheckbox" {{
+                        $goal->completed ? 'checked' : '' }}>
                 </div>
                 <div class="col-md-8">
-                    <h5>{{$goal->goalName}}</h5>
+                    <div class="row">
+                        <h5>{{$goal->goalName}}</h5>
+                        <p>Description: {{$goal->description}} </p>
+                    </div>
                 </div>
                 <div class="col-md-3">
                     <div class="goalDeadline">
                         <p class="ml-4">Due Date: {{$goal->deadline}}</p>
                     </div>
+                    <div class="float-end mt-3">
+                        <a href="" wire:click="editGoalFields({{ $goal->id }})" class="edit btn text-white"
+                            data-bs-toggle="modal" data-bs-target="#editGoalModal" id="pink-colour">Edit</a>
+                        <a href="" wire:click="deleteGoalButton({{ $goal->id }})" class="delete btn btn-danger"
+                            data-bs-toggle="modal" data-bs-target="#deleteGoalModal">Delete</a>
+                    </div>
+        
                 </div>
             </div>
-            <p class="ml-4">Description: {{$goal->description}} </p>
         
-            <div class="actions mt-3">
-                <a href="" wire:click="editGoalFields({{ $goal->id }})" class="edit btn text-white"
-                    data-bs-toggle="modal" data-bs-target="#editGoalModal" id="pink-colour">Edit</a>
-                <a href="" wire:click="deleteGoalButton({{ $goal->id }})" class="delete btn btn-danger"
-                    data-bs-toggle="modal" data-bs-target="#deleteGoalModal">Delete</a>
-            </div>
+        
         </div>
         @endforeach
 
